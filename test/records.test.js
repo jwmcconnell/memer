59ba2b00265bacbb7e1e41731d0007c7941253b1
Jackmc <jackmc1122@gmail.com>
2019-07-16 15:11:15 -0700

    Add a record resource and make a post route for it.

---
+++ test/records.test.js
@@ -0,0 +1,31 @@
+require('./dataHelpers');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('POST record route', () => {
+  it('creates and returns a record', () => {
+    return request(app)
+      .post('/api/v1/records')
+      .send({
+        title: 'How Did We Get So Dark?',
+        artist: 'Royal Blood',
+        year: 2017,
+        color: 'Black',
+        rpm: '33 1/3',
+        size: 12
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'How Did We Get So Dark?',
+          artist: 'Royal Blood',
+          year: 2017,
+          color: 'Black',
+          rpm: '33 1/3',
+          size: 12,
+          __v: 0
+        });
+        expect(res.status).toEqual(200);
+      });
+  });
+});
