70b1d2035b7bdfc34e5633d283f352d1e1188716
Jackmc <jackmc1122@gmail.com>
2019-07-16 12:59:16 -0700

    Add POST memes route.

--- test/app.test.js
+++ test/app.test.js
@@ -21,7 +21,7 @@ describe('GET memes route', () => {
 });
 
 describe('GET meme by id route', () => {
-  it('returns a meme by its meme', async() => {
+  it('returns a meme by its id', async() => {
     const { _id } = await getMeme();
     return request(app)
       .get(`/api/v1/memes/${_id}`)
@@ -31,6 +31,28 @@ describe('GET meme by id route', () => {
           top: expect.any(String),
           bottom: expect.any(String),
           image: expect.any(String),
+          __v: 0
+        });
+        expect(res.status).toEqual(200);
+      });
+  });
+});
+
+describe('POST meme route', () => {
+  it('returns the created meme', () => {
+    return request(app)
+      .post('/api/v1/memes')
+      .send({
+        top: 'top-test',
+        bottom: 'bottom-test',
+        image: 'https://www.pixelstalk.net/wp-content/uploads/2016/06/Meme-Wallpapers.jpg'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          top: 'top-test',
+          bottom: 'bottom-test',
+          image: 'https://www.pixelstalk.net/wp-content/uploads/2016/06/Meme-Wallpapers.jpg',
           __v: 0
         });
         expect(res.status).toEqual(200);
